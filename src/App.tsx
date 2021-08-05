bbb7b0ae8f964df26b80df40569ae1056a0dc39e
홍영설 <hong-yeongseol@hong-yeongseol-ui-MacBook-Pro.local>
2021-08-05 22:15:46 +0900

    경로파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Container, Grid, TextField } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+  }),
+);
+  
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      ><form className={classes.container} noValidate>
+          <TextField
+            id="datetime-local"
+            label="시작 시간"
+            type="datetime-local"
+            defaultValue="2017-05-24T10:30"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form></Grid>
+        
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      ><form className={classes.container} noValidate>
+          <TextField
+            id="datetime-local"
+            label="종료 시간"
+            type="datetime-local"
+            defaultValue="2017-05-24T10:30"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form></Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      ><Button variant="contained" color="primary" href="#contained-buttons">
+          마니또 Start!!
+        </Button></Grid>
+    </Container>
+    
   );
 }
 
---
+++ src/page/guess.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@material-ui/core";
+
+function App() {
+    return (
+        <div>
+            <Container maxWidth="sm">
+                guess Page 입니다.
+            </Container>
+        </div>
+    );
+}
+  
+  export default App;
