f8339f2a1af15354131ab0387e46d9f7cd0c0ce9
john <lord1229@gmail.com>
2021-08-27 18:19:52 +0900

    main 화면 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 import Admin from "./pages/Admin";
 import Main from "./pages/Main";
 import Participants from "./pages/ParticipantJoin";
-import ParticipantResult from "./pages/ParticipantResult"
-import AdminResult from "./pages/AdminResult"
+import ParticipantResult from "./pages/ParticipantResult";
+import AdminResult from "./pages/AdminResult";
+import { CssBaseline } from "@material-ui/core";
 
 function App() {
   return (
     <Router>
+      <CssBaseline />
       <Switch>
         <Route path="/participants/join">
           <Participants />
@@ -17,14 +19,14 @@ function App() {
         <Route path="/participants/result">
           <ParticipantResult />
         </Route>
-        <Route path="/main">
-          <Main />
-        </Route>
         <Route path="/admin/result">
           <AdminResult />
         </Route>
         <Route path="/admin">
           <Admin />
+        </Route>
+        <Route path="/">
+          <Main />
         </Route>
       </Switch>
     </Router>
